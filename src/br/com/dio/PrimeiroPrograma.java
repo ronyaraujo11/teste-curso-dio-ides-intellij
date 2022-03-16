8ff89a39c2eba07cbb18784c634c7f98a0f3a6cc
Rony Araújo <87391175+ronyaraujo11@users.noreply.github.com>
2022-03-16 00:42:04 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+
+        System.out.println(cachorro);
+       /* System.out.println("Olá, me chamo Rony Araújo");*/
+
+    }
+}
